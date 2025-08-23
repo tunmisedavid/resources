f55dd7a048ec8da8f180023d8d4d2fb5407e4f54
Arowolo Tunmise <tunmisearowolo094@gmail.com>
2025-08-23 20:41:22 +0100

    changing widths

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -31,7 +31,7 @@ const Resources = () => {
       fontFamily={"Poppins"}
       marginBottom={{ base: "60px", lg: "100px" }}
     >
-      <Box m={"auto"} marginTop={{ base: "10", lg: "20" }} maxWidth={"1200px"}>
+      <Box m={"auto"} marginTop={{ base: "10", lg: "20" }} maxWidth={"1250px"}>
         <Box margin={"auto"} maxWidth={"750px"}>
           <Text
             color={"#2C3237"}
@@ -59,8 +59,8 @@ const Resources = () => {
           </Text>
           <InputGroup
             m={"auto"}
-            display={'flex'}
-            alignItems={'center'}
+            display={"flex"}
+            alignItems={"center"}
             width={"80%"}
             marginTop={{ base: "8", lg: "10" }}
             startElement={<LuSearch />}
@@ -74,9 +74,31 @@ const Resources = () => {
             />
           </InputGroup>
         </Box>
+        <Box position={"relative"} marginTop={{ base: "70px", lg: "20px" }}>
+          <Flex
+            alignItems={"center "}
+            justify={"center"}
+            gap={4}
+            backgroundColor={"#F2F2F2"}
+            color={"#3F3F3F"}
+            position={"absolute"}
+            w={"100%"}
+            left={0}
+            right={0}
+            height={10}
+            display={{ base: "flex", lg: "none" }}
+          >
+            <IoFilterSharp style={{ fontSize: "25px", fontWeight: "bolder" }} />
+            <Text fontWeight={"medium"} fontFamily={"Poppins"}>
+              Show Filters
+            </Text>
+          </Flex>
+        </Box>
         <Text
           display={{ base: "none", lg: "block" }}
           paddingLeft={5}
+          m={"auto"}
+          width={{ base: "85%", xl: "90%" }}
           marginTop={20}
           fontFamily={"Inter"}
           fontWeight={"bold"}
@@ -84,150 +106,132 @@ const Resources = () => {
         >
           Filters
         </Text>
-        <Flex
-          justifyContent={"space-between"}
-          flexDirection={{ base: "column", lg: "row" }}
-          gap={10}
-          marginTop={{ base: "60px", md: "70px", lg: "20px" }}
-          position={"relative"}
-        >
-          <Box>
+        <Box m={"auto"} width={{ base: "85%", xl: "90%" }}>
+          <Flex
+            justifyContent={"space-between"}
+            flexDirection={{ base: "column", lg: "row" }}
+            gap={10}
+            marginTop={{ base: "120px", md: "110px", lg: "20px" }}
+            position={"relative"}
+          >
             <Box
               display={{ base: "none", lg: "block" }}
-              width={258}
+              width={368}
               padding={5}
               borderTopWidth={1}
               borderTopColor={"#E0E0E0"}
             >
               <SideCheckbox />
             </Box>
-            <Flex
-              alignItems={"center "}
-              justify={"center"}
-              gap={4}
-              backgroundColor={"#F2F2F2"}
-              color={"#3F3F3F"}
-              position={"absolute"}
-              left={0}
-              right={0}
-              height={10}
-              display={{ base: "flex", lg: "none" }}
+
+            <Box
+              margin={"auto"}
+              marginTop={{ base: "35px", md: "50px", lg: "0" }}
+              width={{ base: "100%" }}
             >
-              <IoFilterSharp
-                style={{ fontSize: "25px", fontWeight: "bolder" }}
-              />
-              <Text fontWeight={"medium"} fontFamily={"Poppins"}>
-                Show Filters
-              </Text>
-            </Flex>
-          </Box>
-          <Box
-            margin={"auto"}
-            marginTop={{ base: "35px", md: "50px", lg: "0" }}
-            width={{ base: "90%", lg: "80%" }}
-          >
-            <Grid
-              templateColumns={{ lg: "repeat(2, 1fr)", xl: "repeat(3, 1fr)" }}
-              placeItems={{ base: "center" }}
-              w={"100%"}
-              gap={4}
-            >
-              <Box position={"relative"} w={"100%"} margin={"auto"}>
-                <GridcardsInner
-                  image={CardImage1}
-                  title="The ultimate guide to Workplace Chat"
-                  info="Sample Topic"
-                  filterCheck="Secure Base"
-                />
-                <Image
-                  src={Bg1}
-                  position={"absolute"}
-                  right={0}
-                  top={0}
-                  borderTopRightRadius={20}
-                />
-              </Box>
-              <Box position={"relative"} w={"100%"} margin={"auto"}>
-                <GridcardsInner
-                  image={CardImage1}
-                  title="The ultimate guide to Workplace Chat"
-                  info="Sample Topic"
-                  filterCheck="Wellbeing"
-                />
-                <Image
-                  src={Bg2}
-                  position={"absolute"}
-                  left={0}
-                  top={0}
-                  borderTopRightRadius={20}
-                  borderTopLeftRadius={20}
-                />
-              </Box>
-              <Box position={"relative"} w={"100%"} margin={"auto"}>
-                <GridcardsInner
-                  image={CardImage2}
-                  title="The ultimate guide to Workplace Chat"
-                  info="Sample Topic"
-                  filterCheck="Sense of Appreciation"
-                />
-                <Image
-                  src={Bg3}
-                  position={"absolute"}
-                  right={0}
-                  top={0}
-                  borderTopRightRadius={20}
-                />
-              </Box>
-              <Box position={"relative"} w={"100%"} margin={"auto"}>
-                <GridcardsInner
-                  image={CardImage2}
-                  title="The ultimate guide to Workplace Chat"
-                  info="Sample Topic"
-                  filterCheck="Secure Base"
-                />
-                <Image
-                  src={Bg4}
-                  position={"absolute"}
-                  left={0}
-                  top={0}
-                  borderTopRightRadius={20}
-                  borderTopLeftRadius={20}
-                />
-              </Box>
-              <Box position={"relative"} w={"100%"} margin={"auto"}>
-                <GridcardsInner
-                  image={CardImage3}
-                  title="The ultimate guide to Workplace Chat"
-                  info="Sample Topic"
-                  filterCheck="Learning Organisation"
-                />
-                <Image
-                  src={Bg5}
-                  position={"absolute"}
-                  left={0}
-                  top={0}
-                  borderTopRightRadius={20}
-                  borderTopLeftRadius={20}
-                />
-              </Box>
-              <Box position={"relative"} w={"100%"} margin={"auto"}>
-                <GridcardsInner
-                  image={CardImage3}
-                  title="The ultimate guide to Workplace Chat"
-                  info="Sample Topic"
-                  filterCheck="Mission and Vision"
-                />
-                <Image
-                  src={Bg6}
-                  position={"absolute"}
-                  right={0}
-                  top={0}
-                  borderTopRightRadius={20}
-                />
-              </Box>
-            </Grid>
-          </Box>
-        </Flex>
+              <Grid
+                templateColumns={{ lg: "repeat(2, 1fr)", xl: "repeat(3, 1fr)" }}
+                placeItems={{ base: "center" }}
+                width={"100%"}
+                gap={4}
+              >
+                <Box position={"relative"} w={"100%"} margin={"auto"}>
+                  <GridcardsInner
+                    image={CardImage1}
+                    title="The ultimate guide to Workplace Chat"
+                    info="Sample Topic"
+                    filterCheck="Secure Base"
+                  />
+                  <Image
+                    src={Bg1}
+                    position={"absolute"}
+                    right={0}
+                    top={0}
+                    borderTopRightRadius={20}
+                  />
+                </Box>
+                <Box position={"relative"} w={"100%"} margin={"auto"}>
+                  <GridcardsInner
+                    image={CardImage1}
+                    title="The ultimate guide to Workplace Chat"
+                    info="Sample Topic"
+                    filterCheck="Wellbeing"
+                  />
+                  <Image
+                    src={Bg2}
+                    position={"absolute"}
+                    left={0}
+                    top={0}
+                    borderTopRightRadius={20}
+                    borderTopLeftRadius={20}
+                  />
+                </Box>
+                <Box position={"relative"} w={"100%"} margin={"auto"}>
+                  <GridcardsInner
+                    image={CardImage2}
+                    title="The ultimate guide to Workplace Chat"
+                    info="Sample Topic"
+                    filterCheck="Sense of Appreciation"
+                  />
+                  <Image
+                    src={Bg3}
+                    position={"absolute"}
+                    right={0}
+                    top={0}
+                    borderTopRightRadius={20}
+                  />
+                </Box>
+                <Box position={"relative"} w={"100%"} margin={"auto"}>
+                  <GridcardsInner
+                    image={CardImage2}
+                    title="The ultimate guide to Workplace Chat"
+                    info="Sample Topic"
+                    filterCheck="Secure Base"
+                  />
+                  <Image
+                    src={Bg4}
+                    position={"absolute"}
+                    left={0}
+                    top={0}
+                    borderTopRightRadius={20}
+                    borderTopLeftRadius={20}
+                  />
+                </Box>
+                <Box position={"relative"} w={"100%"} margin={"auto"}>
+                  <GridcardsInner
+                    image={CardImage3}
+                    title="The ultimate guide to Workplace Chat"
+                    info="Sample Topic"
+                    filterCheck="Learning Organisation"
+                  />
+                  <Image
+                    src={Bg5}
+                    position={"absolute"}
+                    left={0}
+                    top={0}
+                    borderTopRightRadius={20}
+                    borderTopLeftRadius={20}
+                  />
+                </Box>
+                <Box position={"relative"} w={"100%"} margin={"auto"}>
+                  <GridcardsInner
+                    image={CardImage3}
+                    title="The ultimate guide to Workplace Chat"
+                    info="Sample Topic"
+                    filterCheck="Mission and Vision"
+                  />
+                  <Image
+                    src={Bg6}
+                    position={"absolute"}
+                    right={0}
+                    top={0}
+                    borderTopRightRadius={20}
+                  />
+                </Box>
+              </Grid>
+            </Box>
+          </Flex>
+        </Box>
       </Box>
     </Box>
   );
